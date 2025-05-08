2fbdbe478173e868278b4967c2306c50ea21fa45
RounitSrivastava <rounit.kumar2702@gmail.com>
2025-05-08 19:06:12 +0530

    sign

--- client/src/app/(sub-pages)/WhatOurCustomers/index.jsx
+++ client/src/app/(sub-pages)/WhatOurCustomers/index.jsx
@@ -2,8 +2,8 @@
 import Carousel from "../../(components)/Carousel";
 const WhatOurCustomersHaveToSay = () => {
   return (
-    <div className="pb-[3vw] pt-[1vh] lg:pb-[2rem] lg:pt-[0rem] text-center bg-[#ECE5DD]"> {/* Reduced bottom padding */}
-      <div className="text-[4vw] lg:text-[3rem] lg:pl-9 font-bold text-black">
+    <div className="pb-[1vw] pt-[0vh] lg:pb-[2rem] lg:pt-[0rem] text-center bg-[#ECE5DD]"> {/* Reduced bottom padding */}
+      <div className="text-[6vw] lg:text-[3rem] lg:pl-9 font-bold text-black">
         What Our <span className="text-[#97571c]">Customers Have to Say</span>
       </div>
       <div className="pt-[2vw]">
