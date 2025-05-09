d7ca91b400d07af7f48977b9c5c43bab26956833
RounitSrivastava <rounit.kumar2702@gmail.com>
2025-05-09 15:50:46 +0530

    message

--- client/src/app/(sub-pages)/VideoImageCarousel/index.jsx
+++ client/src/app/(sub-pages)/VideoImageCarousel/index.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import { Play } from "lucide-react";
 
 const VideoImageCarousel = () => {
-  const videos = [
-    { id: 1, src: "/MVI_6792.MP4", title: "THE MAKERS OF" },
-    { id: 2, src: "/MVI_6817.MP4", title: "THE MAKING OF" },
-    { id: 3, src: "/MVI_6847.MP4", title: "Horizons" },
-    { id: 4, src: "/MVI_6882.MP4", title: "Laxmi Ben" },
-  ];
-
   const images = [
-    { id: 1, src: "/IMG_6812.jpg" },
-    { id: 2, src: "/IMG_6866.jpg" },
-    { id: 3, src: "/IMG_6914.jpg" },
-    { id: 4, src: "/IMG_6972.jpg" },
+    { id: 1, src: "./IMG_6812.jpg" },
+    { id: 2, src: "./image2.jpg" },
+    { id: 3, src: "./image3.jpg" },
+    { id: 4, src: "./image4.jpg" },
   ];
 
   return (
     <div className="p-4 space-y-4">
-      <div className="grid grid-cols-4 gap-4">
-        {videos.map((video) => (
-          <div key={video.id} className="relative group">
-            <video src={video.src} alt={video.title} className="w-full h-full object-cover" controls />
-          </div>
-        ))}
-      </div>
-
       <div className="grid grid-cols-4 gap-4">
         {images.map((image) => (
           <div key={image.id}>
